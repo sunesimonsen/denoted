3fdaa66aecdc3637a20e00fcf23d5d467ba28937
Sune Simonsen <sune@we-knowhow.dk>
2023-12-31 10:16:34 +0100

    Clear search button

--- public/components/FileSearch.js
+++ public/components/FileSearch.js
@@ -1,19 +1,30 @@
 import { html } from "@dependable/view";
 import { MediaInput } from "@dependable/components/MediaInput/v0";
-import { me2, ms2 } from "@dependable/components/spacing/v0";
+import { ms2, me2 } from "@dependable/components/spacing/v0";
 import { searchText } from "../state.js";
 
 import SearchStroke16Icon from "@dependable/icons/SearchStroke16Icon";
+import XStroke12Icon from "@dependable/icons/XStroke12Icon";
 
 export class FileSearch {
   constructor() {
     this.onInput = (e) => {
       searchText(e.target.value);
     };
+
+    this.onClear = () => {
+      searchText("");
+    };
   }
+
+  renderClear() {
+    return html`<${XStroke12Icon} className=${ms2} onClick=${this.onClear} />`;
+  }
+
   render() {
     return html`
       <${MediaInput}>
+        <${SearchStroke16Icon} className=${me2} />
         <input
           id="fileSearch"
           autocomplete="off"
@@ -21,7 +32,7 @@ export class FileSearch {
           .value=${searchText()}
           onInput=${this.onInput}
         />
-        <${SearchStroke16Icon} className=${ms2} />
+        ${searchText() && this.renderClear()}
       <//>
     `;
   }
