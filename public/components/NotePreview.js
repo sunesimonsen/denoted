5922f143ed01df0afb7063b9aa05220ea70c6d6d
Sune Simonsen <sune@we-knowhow.dk>
2023-12-31 10:16:57 +0100

    Style note

--- public/components/NotePreview.js
+++ public/components/NotePreview.js
@@ -17,12 +17,55 @@ const styles = css`
   & {
     background: var(--dc-color-neutral-0);
     border: thin solid var(--dc-color-neutral-2);
-    padding: 30px;
+    padding: 50px 60px;
     margin: 30px auto;
     width: 800px;
     min-height: 1200px;
     overflow-x: hidden;
     overflow-wrap: break-word;
+  }
+
+  & a {
+    text-decoration: none;
+    color: var(--dc-color-primary-0);
+  }
+
+  & a:focus,
+  & a:hover {
+    outline: none;
+    text-decoration: underline;
+    color: var(--dc-color-primary-1);
+  }
+
+  & a:active {
+    text-decoration: underline;
+    color: var(--dc-color-primary-2);
+  }
+
+  & h1,
+  & h2,
+  & h3 {
+    font-weight: normal;
+    margin: 0;
+  }
+
+  & h1 {
+    font-size: 1.6em;
+    margin-bottom: 0.6em;
+  }
+
+  & h2 {
+    font-size: 1.4em;
+    margin-bottom: 0.4em;
+  }
+
+  & h3 {
+    font-size: 1.2em;
+    margin-bottom: 0.2em;
+  }
+
+  & p {
+    margin: 0.5em 0;
   }
 `;
 
