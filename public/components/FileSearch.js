8693cae1200001e2e2d0692756296a2a8ae1e54e
Sune Simonsen <sune@we-knowhow.dk>
2024-01-04 16:00:58 +0100

    Use SearchInput component

--- public/components/FileSearch.js
+++ public/components/FileSearch.js
@@ -1,39 +1,23 @@
 import { html } from "@dependable/view";
-import { MediaInput } from "@dependable/components/MediaInput/v0";
-import { ms2, me2 } from "@dependable/components/spacing/v0";
+import { SearchInput } from "@dependable/components/SearchInput/v0";
 import { searchText } from "../state.js";
 
-import SearchStroke16Icon from "@dependable/icons/SearchStroke16Icon";
-import XStroke12Icon from "@dependable/icons/XStroke12Icon";
+const onInput = (e) => {
+  searchText(e.target.value);
+};
+
+const onClear = () => {
+  searchText("");
+};
 
 export class FileSearch {
-  constructor() {
-    this.onInput = (e) => {
-      searchText(e.target.value);
-    };
-
-    this.onClear = () => {
-      searchText("");
-    };
-  }
-
-  renderClear() {
-    return html`<${XStroke12Icon} className=${ms2} onClick=${this.onClear} />`;
-  }
-
   render() {
     return html`
-      <${MediaInput}>
-        <${SearchStroke16Icon} className=${me2} />
-        <input
-          id="fileSearch"
-          autocomplete="off"
-          type="text"
-          .value=${searchText()}
-          onInput=${this.onInput}
-        />
-        ${searchText() && this.renderClear()}
-      <//>
+      <${SearchInput}
+        .value=${searchText()}
+        onInput=${onInput}
+        onClear=${onClear}
+      />
     `;
   }
 }
