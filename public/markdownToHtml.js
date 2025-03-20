eabdc6f0b3595ea6cd3034cecee19b9db6254145
Sune Simonsen <sune@we-knowhow.dk>
2025-03-20 17:44:50 +0100

    Auto link

--- public/markdownToHtml.js
+++ public/markdownToHtml.js
@@ -1,3 +1,5 @@
 import { marked } from "marked";
+import linkifyIt from "marked-linkify-it";
+marked.use(linkifyIt);
 
-export const markdownToHtml = marked.parse;
+export const markdownToHtml = marked;
